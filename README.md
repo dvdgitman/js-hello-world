425e8fdc87abb632d66cbe0dfcc714497a2a3325
dvdgitman <wereking@gmail.com>
2023-07-23 15:44:57 +0300

    added eslint

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # js-hello-world
-Here I will do a JS demo project 
+Here I will do a JS demo project
--- helloworld.js
+++ helloworld.js
@@ -1,10 +1,10 @@
 // Import the built-in 'readline' module to read user input from the command line
-const readline = require('readline');
+const readline = require("readline");
 
 // Create a readline interface to interact with the user via the command line
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 // Functions to perform basic arithmetic operations
@@ -27,25 +27,25 @@ function divide(a, b) {
 // Function to calculate the result based on the selected operator
 function calculate(operator, a, b) {
   switch (operator) {
-    case '+':
+    case "+":
       return add(a, b);
-    case '-':
+    case "-":
       return subtract(a, b);
-    case '*':
+    case "*":
       return multiply(a, b);
-    case '/':
+    case "/":
       return divide(a, b);
     default:
-      return 'Invalid operator';
+      return "Invalid operator";
   }
 }
 
 // Start the interaction with the user by prompting for the first number
-rl.question('Enter the first number: ', (num1) => {
+rl.question("Enter the first number: ", (num1) => {
   // Once the user enters the first number, prompt for the operator
-  rl.question('Enter the operator (+, -, *, /): ', (operator) => {
+  rl.question("Enter the operator (+, -, *, /): ", (operator) => {
     // Once the user enters the operator, prompt for the second number
-    rl.question('Enter the second number: ', (num2) => {
+    rl.question("Enter the second number: ", (num2) => {
       // Once the user enters the second number, calculate the result
       const result = calculate(operator, parseFloat(num1), parseFloat(num2));
       // Display the result to the console
