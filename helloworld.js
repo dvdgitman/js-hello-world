4d1d11742cfc9a9f70d3c20c415a17df4b0342a7
dvdgitman <wereking@gmail.com>
2023-07-24 18:09:33 +0300

    learning-objects

--- helloworld.js
+++ helloworld.js
@@ -1,69 +1,45 @@
-// Import the built-in 'readline' module to read user input from the command line
-const readline = require("readline");
+class Person {
+  name;
+  age;
+  sex;
 
-// Create a readline interface to interact with the user via the command line
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+  constructor(name, age, sex) {
+    this.name = name;
+    this.age = age;
+    this.sex = sex;
+  }
 
-// Functions to perform basic arithmetic operations
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-// Function to calculate the result based on the selected operator
-function calculate(operator, a, b) {
-  switch (operator) {
-    case "+":
-      return add(a, b);
-    case "-":
-      return subtract(a, b);
-    case "*":
-      return multiply(a, b);
-    case "/":
-      return divide(a, b);
-    default:
-      return "Invalid operator";
+  transgender() {
+    if (this.sex == "male") {
+      this.sex = "female";
+    }
+    else if (this.sex == "female"){
+      this.sex = "male";
+    }
+    else {
+      this.sex = "cocksi";
+    }
   }
 }
 
-function getInput(readlineInterface, prompt) {
-  return new Promise(resolve => {
-    readlineInterface.question(prompt, answer => {
-      resolve(answer);
+const maslanikov = new Person("Daniel Maslanikov", 29, "male");
+console.log(maslanikov);
+maslanikov.transgender()
+console.log(maslanikov);
+maslanikov.transgender()
+console.log(maslanikov);
 
-      // Close the readline interface
-      readlineInterface.close();
-    })
-  })
+
+let killme = {
+  name: "David",
+  lastname: "Gitman",
+  age: 16,
+  sayHello: function() {
+    console.log(`Hello, my name is ${this.name}`)
+  }
 }
 
-(async function main() {
-  console.log("Hello!");
+console.log(killme.name)
 
-  // Start the interaction with the user by prompting for the first number
-  const num1 = await getInput(rl, "Enter the first number: ");
-
-  // Once the user enters the first number, prompt for the operator
-  const operator = await getInput(rl, "Enter the operator (+, -, *, /): ");
-
-  // Once the user enters the operator, prompt for the second number
-  const num2 = await getInput(rl, "Enter the second number: ");
-
-  // Once the user enters the second number, calculate the result
-  const result = calculate(operator, parseFloat(num1), parseFloat(num2));
-  console.log(`Result: ${result}`);
-})();
+killme.name = "fuckmedaddy"
+killme.sayHello()
